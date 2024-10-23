eb013ad1ad3130a63b063e80c601cd3ba5d76bbc
JS Web Coder <lightbeam404@gmail.com>
2024-10-23 03:56:37 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>Xplore App</div>
+  );
+};
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev3/config/
+export default defineConfig({
+  plugins: [react()],
+});
